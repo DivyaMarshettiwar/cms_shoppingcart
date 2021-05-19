a8c6dc99d33d28036fc279eb552cae53d3caca00
DivyaMarshettiwar <65171554+DivyaMarshettiwar@users.noreply.github.com>
2021-05-19 10:48:59 -0700

    initial commit creating the project

---
+++ cmsshoppingcart/src/test/java/com/cms/shoppingcart/CmsshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsshoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
