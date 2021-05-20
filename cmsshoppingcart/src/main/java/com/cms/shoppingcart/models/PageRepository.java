ad45e8af7452b5fdd9e72367abbd7a2c374050a8
DivyaMarshettiwar <65171554+DivyaMarshettiwar@users.noreply.github.com>
2021-05-20 04:47:08 -0700

    updating the admin pages

--- cmsshoppingcart/src/main/java/com/cms/shoppingcart/models/PageRepository.java
+++ cmsshoppingcart/src/main/java/com/cms/shoppingcart/models/PageRepository.java
@@ -9,4 +9,11 @@ import com.cms.shoppingcart.models.data.Page;
 public interface PageRepository extends JpaRepository<Page, Integer>{
 
 	List<Page> findAll();
+	
+	Page findBySlug(String slug);
+	
+	Page findBySlugAndIdNot(String slug, int id);
+	
+	// this is for page reordering
+	List<Page> findAllByOrderBySortingAsc();
 }
---
+++ cmsshoppingcart/src/main/resources/static/js/app.js
@@ -0,0 +1,9 @@
+$( function() {
+	
+	$("a.confirmDeletion").click(function(){
+		
+		if(!confirm("confirm deletion")) return false;
+		
+	});
+	
+});
